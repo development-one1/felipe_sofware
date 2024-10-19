7d0b8c82507f44ccbd8900eac39ac691c5ac5b1c
Andrés Felipe <camachorodriguezandresfelipe@gmail.com>
2024-10-19 22:08:13 +0200

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import fd from './assets/fd.jpg'
 import './App.css'
 
 function App() {
@@ -8,26 +7,36 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <header>
+        <div className='icono'>
+          <a href="https://development-one1.github.io/felipe_sofware/" target="_blank">
+            <img src={fd} className="logo" alt="felipe develop" />
+          </a>
+          <h1 className='texto'>Felipe_Develop</h1>
+        </div>
+      </header>
+
+        <section>
+              <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+                </button>
+                <p>
+                  Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+              </div>
+        </section>
+
+      <footer>
+        <p className="read-the-docs">
+          Portafolio
+          <a href='https://github.com/development-one1/pokemons-redux/'>
+          Pokemons
+          </a>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+        <p>&copy; 2025 Felipe_develop. Todos los derechos reservados.</p>
+      </footer>
     </>
   )
 }
